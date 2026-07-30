661d1f7d5afce2f1806881e1a0bbadbfffe38bb6
Shiyan-hm <huang10111921@gmail.com>
2026-07-30 22:35:30 +0800

    Create task.py

---
+++ mila-api/app/models/task.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column,Integer,String,Boolean
+
+from app.database import Base
+
+
+
+class Task(Base):
+
+
+    __tablename__="tasks"
+
+
+
+    id=Column(
+    Integer,
+    primary_key=True
+    )
+
+
+    title=Column(
+    String
+    )
+
+
+    subject=Column(
+    String
+    )
+
+
+    finished=Column(
+    Boolean,
+    default=False
+    )
