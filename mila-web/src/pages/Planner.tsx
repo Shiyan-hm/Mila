f92afddcabeeddd1b02ac727cd2d55c8c4799b41
Shiyan-hm <huang10111921@gmail.com>
2026-07-30 20:25:34 +0800

    Add Planner component with daily schedule

---
+++ mila-web/src/pages/Planner.tsx
@@ -0,0 +1,47 @@
+import Card from "../components/Card";
+
+
+export default function Planner(){
+
+
+return(
+
+<Card>
+
+<h1>
+
+📅 今日计划
+
+</h1>
+
+
+<ul>
+
+<li>
+08:00 数学 极限
+</li>
+
+
+<li>
+10:30 英语阅读
+</li>
+
+
+<li>
+14:00 408 数据结构
+</li>
+
+
+<li>
+19:00 政治背诵
+</li>
+
+
+</ul>
+
+
+</Card>
+
+)
+
+}
