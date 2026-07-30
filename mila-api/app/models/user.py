36d203498b2ada274188721d8d611864a4cca904
Shiyan-hm <huang10111921@gmail.com>
2026-07-30 22:34:30 +0800

    Create User model for database

---
+++ mila-api/app/models/user.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Column,Integer,String
+
+from app.database import Base
+
+
+class User(Base):
+
+    __tablename__="users"
+
+
+    id=Column(
+    Integer,
+    primary_key=True
+    )
+
+
+    username=Column(
+    String,
+    unique=True
+    )
+
+
+    password=Column(
+    String
+    )
+
+
+    target_school=Column(
+    String
+    )
+
+
+    target_major=Column(
+    String
+    )
