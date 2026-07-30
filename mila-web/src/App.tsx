64d4ba75e578c1b7e420c3e27ba68aedf7f4f5f4
Shiyan-hm <huang10111921@gmail.com>
2026-07-30 20:14:21 +0800

    Add routing and sidebar to App component

--- mila-web/src/App.tsx
+++ mila-web/src/App.tsx
@@ -1,1 +1,38 @@
+import {
 
+BrowserRouter
+
+} from "react-router-dom";
+
+
+import Router from "./router";
+
+
+import Sidebar from "./components/Sidebar";
+
+
+export default function App(){
+
+
+return(
+
+<BrowserRouter>
+
+
+<div className="flex">
+
+
+<Sidebar/>
+
+
+<Router/>
+
+
+</div>
+
+
+</BrowserRouter>
+
+)
+
+}
